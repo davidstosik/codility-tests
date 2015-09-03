ac44fd2b2d430609862f6a8fd32183e0a9b16fc7
David Stosik <david.stosik+git-noreply@gmail.com>
2015-09-03 20:05:52 +0900

    Fix task 3.

--- task3.rb
+++ task3.rb
@@ -1,5 +1,8 @@
 # (1, 0, 1, 5, 0, 0, 0, 0) => false
 # (1, 0, 1, 5, 0, 0, 2, 4) => true
+# (1,0,1,1,0,0,-1,0) => false
+# (1,0,2,0,0,0,3,0) => true
+#
 def solution(k, l, m, n, p, q, r, s)
     if p == r # segment 2 is vertical
         t, u, v, w = p, q, r, s
@@ -10,13 +13,14 @@ def solution(k, l, m, n, p, q, r, s)
     if k == m # at least segment 1 is vertical
         # note: could flip the plan and call again with horizontal lines
         if p == r # segment 2 is too
-            return (k == p) && (l <= q && q <= n || l <= s && s <= n) # same X && partly common
+            return (k == p) && !(q>[l,n].max && q>[l,n].max || q<[l,n].min && s<[l,n].min) # same X && partly common
         else # segment 2 is not vertical, so crosses
+          puts 'in here'
             a2 = (s-q).to_f / (r-p)
             b2 = q - a2 * p
             x = k
             y = a2 * x + b2
-            return l <= y && y <= n && q <= y && y <= s
+            return between(x, y, k, l, m, n, p, q, r, s)
         end
     else # none of the segments is vertical
         a1 = (n-l).to_f / (m-k)
@@ -26,12 +30,18 @@ def solution(k, l, m, n, p, q, r, s)
 
         #a1.x + b1 = a2.x + b2
         if a1 == a2 # parallel lines
-            return (b1 == b2) && (k <= p && p <= m || k <= r && r <= m) # same line && partly common
+            return (b1 == b2) && !(p>[k,m].max && r>[k,m].max || p<[k,m].min && r<[k,m].min) # same line && partly common
         else # crossing lines
             x = (b2 - b1) / ( a1 - a2)
-            return k <= x && x <= m && p <= x && x <= r
+            y = a1 * x + b1
+            return between(x, y, k, l, m, n, p, q, r, s)
         end
     end
 
     false
+end
+
+def between(x, y, k, l, m, n, p, q, r, s)
+    k <= x && x <= m && p <= x && x <= r && \
+    l <= y && y <= n && q <= y && y <= s
 end
